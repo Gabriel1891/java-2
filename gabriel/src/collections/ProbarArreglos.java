a88ef6651d3b827db943ff0b51d643b6d68b528e
T-107 <T-107@PC263>
2015-07-13 19:44:45 -0500

    Guia Inicio Proyecto Final

---
+++ gabriel/src/collections/ProbarArreglos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author T-107
+ */
+public class ProbarArreglos {
+    
+    public static void main(String[] args) {
+    GeneradorDeUsuarios generadora=new GeneradorDeUsuarios();
+                        generadora.agregarUsuario("Chana", 18 , "chana@gmail.com");
+    ArrayList<Usuario>usuarios=(ArrayList<Usuario>)generadora.getUsuarios();
+                      System.out.println(usuarios.size());
+    for(Usuario u:usuarios){
+        System.out.println(u.getNombre()+" Edad:"+u.getEdad()+" email: "+u.getEmail());
+    }
+                      
+    }
+}
